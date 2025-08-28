af6f567230a9cdb317337625fed748964640dcb6
vikraman1653763 <vikraman1653763@gmail.com>
2025-08-28 14:36:00 +0530

    28-08-2025

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import ProfilePage from './pages/ProfilePage'
-import LoginPage from './pages/LoginPage'
+import React, { useContext } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import LoginPage from "./pages/LoginPage";
+import { Toaster } from "react-hot-toast";
+import { AuthContext } from "../context/AuthContext";
 
 const App = () => {
- 
+  const { authUser } = useContext(AuthContext);
 
- 
   return (
     <div className="bg-[url('./src/assets/bgImage.svg')] bg-cover">
+      <Toaster />
       <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/login' element={<LoginPage/>}/>
-        <Route path='/profile' element={<ProfilePage/>}/>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,7 +1,10 @@
-import React, { use } from "react";
+import React, { use, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import assets, { userDummyData } from "../assets/assets";
+import { AuthContext } from "../../context/AuthContext";
 const SideBar = ({ selectedUser, setSelectedUser }) => {
+  
+  const{logout} = useContext(AuthContext)
   const navigate = useNavigate();
 
   return (
@@ -27,7 +30,7 @@ const SideBar = ({ selectedUser, setSelectedUser }) => {
                 Edit Profile
               </p>
               <hr className="my-2 border-t border-gray-500" />
-              <p className=" cursor-pointer text-sm">Logout</p>
+              <p onClick={()=>logout()} className=" cursor-pointer text-sm">Logout</p>
             </div>
           </div>
         </div>
